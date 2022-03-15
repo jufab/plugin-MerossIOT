8216371fbb30203a11db864c1fb0982a056d0a14
Julien Fabre <julien.fabre.33@gmail.com>
2022-03-15 23:31:51 +0100

    fix: keyError

--- resources/MerossIOTd/meross.py
+++ resources/MerossIOTd/meross.py
@@ -193,7 +193,7 @@ class MerossCoordinator:
 
     async def get_devices_meross(self):
         devices = self.find_devices()
-        logging.debug("liste des devices : {}".format(devices))
+        logging.debug(f"liste des devices : {devices}")
         return await asyncio.gather(
             *(get_one_device_meross(device) for device in devices))
 
@@ -321,7 +321,7 @@ async def get_one_device_meross(device):
             pass
     d[device.uuid]['values']['switch'] = switch
     # Puissance
-    if Namespace.CONTROL_ELECTRICITY in data:
+    if Namespace.CONTROL_ELECTRICITY in data.keys():
         d[device.uuid]['elec'] = True
         electricity = await device.async_get_instant_metrics()
         d[device.uuid]['values']['power'] = electricity.power
@@ -330,7 +330,7 @@ async def get_one_device_meross(device):
     else:
         d[device.uuid]['elec'] = False
     # Consommation
-    if Namespace.CONTROL_CONSUMPTIONX in data or Namespace.CONTROL_CONSUMPTION in data:
+    if Namespace.CONTROL_CONSUMPTIONX in data.keys() or Namespace.CONTROL_CONSUMPTION in data.keys():
         d[device.uuid]['conso'] = True
         l_conso = await device.async_get_daily_power_consumption()
         d[device.uuid]['values']['conso_totale'] = 0
@@ -342,7 +342,7 @@ async def get_one_device_meross(device):
     else:
         d[device.uuid]['conso'] = False
     # Lumiere
-    if Namespace.CONTROL_LIGHT in data:
+    if Namespace.CONTROL_LIGHT in data.keys():
         d[device.uuid]['light'] = True
         d[device.uuid]['lumin'] = device.get_supports_luminance()
         d[device.uuid]['tempe'] = device.get_supports_temperature()
@@ -359,7 +359,7 @@ async def get_one_device_meross(device):
         d[device.uuid]['tempe'] = False
         d[device.uuid]['isrgb'] = False
     # HUMIDIFIER
-    if Namespace.CONTROL_SPRAY in data:
+    if Namespace.CONTROL_SPRAY in data.keys():
         d[device.uuid]['spray'] = True
         # d[device.uuid]['values']['spray'] = device.get_spray_mode().value
     else:
