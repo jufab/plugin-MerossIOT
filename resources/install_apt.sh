ed8e76728cddf283e44f2d254832a5c261506c83
Julien Fabre <julien.fabre.33@gmail.com>
2024-11-18 02:04:00 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -19,9 +19,9 @@ echo 60 > ${PROGRESS_FILE}
 sudo apt-get install -y python3-dev
 echo 70 > ${PROGRESS_FILE}
 BASEDIR=$(dirname "$0")
-sudo python3 -m pip install httpx
+#sudo python3 -m pip install httpx
 meross_version=$(head -1 $BASEDIR/meross-iot_version.txt)
-sudo python3 -m pip install meross_iot==$meross_version
+#sudo python3 -m pip install meross_iot==$meross_version
 echo 100 > ${PROGRESS_FILE}
 echo "Installation des dépendances terminée !"
 rm ${PROGRESS_FILE}
