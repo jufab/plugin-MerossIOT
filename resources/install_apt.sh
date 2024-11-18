455ecee831194d3a4975e117a480fdca70902bf1
Julien Fabre <julien.fabre.33@gmail.com>
2024-11-18 01:43:48 +0100

    Update install_apt.sh

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -19,6 +19,7 @@ echo 60 > ${PROGRESS_FILE}
 sudo apt-get install -y python3-dev
 echo 70 > ${PROGRESS_FILE}
 BASEDIR=$(dirname "$0")
+sudo python3 -m pip install certifi==2024.7.4 --user
 sudo python3 -m pip install httpx --user
 meross_version=$(head -1 $BASEDIR/meross-iot_version.txt)
 sudo python3 -m pip install meross_iot==$meross_version --user
